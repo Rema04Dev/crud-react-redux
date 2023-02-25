20d0189b55aae0e9d734a9b6711fb5762bc1ae1a
Roman <romansmerdyakov@gmail.com>
2023-02-25 18:36:56 +0600

    update TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,30 @@
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, CloseButton } from 'react-bootstrap';
+const TodoItem = ({item}) => {
+  const { title, priority} = item;
+  const cssMapping = {
+    'high': 'text-danger',
+    'middle': 'text-primary',
+    'low': 'text-success'
+  };
+  const itemCss = cssMapping[priority]
+
+  return(
+    <ListGroup.Item className={itemCss}>{title} <CloseButton /> </ListGroup.Item>
+  );
+}
 
 const TodoList = () => {
+  const todos = [{id: 1, title: 'buy milk', priority: 'middle'}]
+
+  if (todos.length === 0) {
+    return <h3 className='mt-3'>List is emty yet...</h3>
+  }
+  
   return (
     <ListGroup className='mt-3'>
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      {
+        todos.map((item) => <TodoItem key={item.id} item={item}/>)
+      }
     </ListGroup>
   )
 }
